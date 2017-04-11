a195711eef51e58c6fb9593aed9eaf16991cd6a2
Felix Silberstein <fxarte@gmail.com>
2017-04-11 17:01:13 -0400

    Fixed issues with csv loader. Improved code

--- csv2es.py
+++ csv2es.py
@@ -11,29 +11,7 @@ cached_values={}
 def validate_item(item):
     return item
 
-    
 
-def get_csv_header(config, refresh=False):
-    ''' need to get the header info in the order they appear in the file, 
-    python dict doesnt garantees the order
-    '''
-    global cached_values
-    if 'csv_header' not in cached_values or refresh:
-        header = config.get('csv', 'header')
-        if header:
-            try:
-                header = int(header)
-                source_path = config.get('main', 'source_path')
-                field_separator = config.get('csv', delimiter)
-                with open(source_path, encoding='utf-8') as sourcefile:
-                    for i, line in enumerate(sourcefile):
-                        if i==header:
-                            header = line.split(field_separator)
-            except:
-                header = header.split(",")
-        cached_values['csv_header'] = header
-    return cached_values['csv_header']
-    
 def recreate_index(config):
     host = config.get('main', 'host')
     port = config.get('main', 'port')
@@ -49,30 +27,28 @@ def recreate_index(config):
         es.indices.delete(index=_index)
     # index settings
     settings={}
-    header = get_csv_header(config)
-    settings = {'mappings': {_type:{'properties':{k:{'type':'text'} for k in header}}}}
     es.indices.create(index=_index, body=settings)
     return es
     
 def getcontent_csv(config):
+    
     source_path = config.get('main', 'source_path')
     _index = config.get('main', 'index')
     _type = config.get('main', 'type')
-    #TODO TEst for comma
+    fieldnames = config.get('csv', 'header', fallback=None)
+    #Parse field names if set
+    fieldnames=fieldnames.split(',') if fieldnames is not None else None
+    # Added support multi-character represented characters, such as tabs.
     delimiter = str.encode(config.get('csv', 'delimiter'), 'utf-8').decode('unicode_escape')
-    # source_path = os.path.abspath(os.path.join(os.path.dirname(__file__), source_path))
-
-    header = get_csv_header(config)
-    with open(source_path, encoding='utf-8') as sourcefile:
-        datareader = csv.reader(sourcefile, delimiter=delimiter)
-
-        for row in datareader:
-            source_dict = {header[ind]:row[ind] for ind, x in enumerate(row)}
+    
+    with open(source_path, encoding='utf-8') as f:
+        csvreader = csv.DictReader(f, fieldnames=fieldnames, delimiter=delimiter)
+        for row in csvreader:
             yield {
                 '_op_type': 'index',
                 '_index': _index,
                 '_type': _type,
-                '_source': source_dict
+                '_source': row
             }
 
 
@@ -100,5 +76,6 @@ def csv2es(parallel=True):
     
 
 if __name__ == '__main__':
-    csv2es(parallel=True)
+    #csv2es(parallel=True)
+    csv2es()
 
